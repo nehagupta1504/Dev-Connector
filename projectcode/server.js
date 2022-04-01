a383166a4977ffc9349e5b37ff92f3bbfffdfc14
Neha Gupta <nehagupta150498@gmail.com>
2022-04-01 20:37:47 +0530

    deployment

--- projectcode/server.js
+++ projectcode/server.js
@@ -5,6 +5,7 @@ const user = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const auth = require("./routes/api/auth");
 const posts = require("./routes/api/posts");
+const path = require("path");
 
 const app = express();
 
@@ -20,6 +21,15 @@ app.use("/api/users", user);
 app.use("/api/auth", auth);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
+
+// Server static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
